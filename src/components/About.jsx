c2978019335a97b777dbcc7c6358bbc99c6cad46
segundoav <segundoav@hotmail.com>
2024-07-23 11:19:21 -0500

    ultimos cambios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,11 @@ const About = () => {
       <div className="max-w-4xl mx-auto flex items-center">
         <img src={aboutImage} alt="About" className="w-1/2 h-auto rounded-lg" />
         <div className="ml-8">
-          <h2 className="text-1xl font-bold mb-4">¡Hola! mi nombre es Segundo Alvarez,y desde hace 8 años soy Corredor de Seguros autorizada por la Superintendencia de Bancos en Nicaragua 🇳🇮. Aliada comercial de las 5 compañías de seguros establecidas en el país.
+          <h2 className="text-1xl font-bold mb-4">¡Hola! mi nombre es Segundo Alvarez,y desde hace 8 años soy Corredor de Seguros autorizada por la Superintendencia de Bancos y Seguros (SBS) en Peru.
 
 Quiero ayudarte a que podas realmente gozar del presente sin preocupaciones 🍃
 
-A través de esta plataforma digital voy a estar más cerca de vos para que conozcas todo lo que necesitas saber acerca de seguros y podas protegerte a vos y a los tuyos de una manera informada y responsabl</h2>
+A través de esta plataforma digital voy a estar más cerca de ti para que conozcas todo lo que necesitas saber acerca de seguros y podas protegerte a ti y a los tuyos de una manera informada y responsable.</h2>
           <p className="text-lg">
             Codigo de corredor de la SBS: N-4917
           </p>
